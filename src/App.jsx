ed25ffb02767f4eff02af40558ba03f51d085b5a
Hồ Thái Khanh <hothaikhanh@gmail.com>
2024-08-28 22:25:11 +0700

    - Updated Page 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,29 @@ import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import Page from "./components/Page";
 
-import { Environment, OrbitControls, Html, Backdrop } from "@react-three/drei";
+import { Environment, OrbitControls, Html, Backdrop, PerspectiveCamera, CameraControls } from "@react-three/drei";
 import { Suspense } from "react";
 
 import "./App.scss";
 
 function App() {
-    // const computer = useLoader(GLTFLoader, "./src/assets/computer_model/scene.gltf");
-    // const desk = useLoader(GLTFLoader, "./src/assets/metal_desk/scene.gltf");
-    // const lamp = useLoader(GLTFLoader, "./src/assets/table_lamp/scene.gltf");
     const computers = useLoader(GLTFLoader, "./src/assets/old_computers/scene.gltf");
     let [cameraPosition, setCameraPosition] = useState({ x: 0, y: 0, z: 0 });
     let [cameraRotation, setCameraRotation] = useState({ x: 0, y: 0, z: 0 });
 
-    const vertices = new Float32Array([
-        -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
-
-        1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0,
-    ]);
     return (
         <div id="canvas-container">
             <Canvas>
+                {/* <PerspectiveCamera
+                    makeDefault
+                    fov={75}
+                    position={[cameraPosition.x, cameraPosition.y, cameraPosition.z]}
+                    rotation={[cameraRotation.x, cameraRotation.y, cameraRotation.z]}
+                /> */}
+
+                {/* PAGE1 */}
+                <PerspectiveCamera makeDefault position={[-2.7, 0.6, 1.6]} fov={75} rotation={[0.0, 1.2, 0.0]} />
+
                 <Suspense fallback={null}>
                     <ambientLight color="grey" intensity={Math.PI} />
                     <spotLight
@@ -35,20 +37,20 @@ function App() {
                         decay={0.1}
                         intensity={Math.PI * 2}
                     />
-                    {/* <pointLight color="white" position={[-10, -10, -10]} decay={0} intensity={Math.PI} /> */}
-                    <primitive scale={1} position={[0, -3, 0]} object={computers.scene} />
-                    {/* <primitive scale={80} position={[0, -65, -10]} object={desk.scene} />
-                    <primitive
-                        scale={4}
-                        position={[-35, -27, 5]}
-                        rotation={[Math.PI / 1, -5, 3.14]}
-                        object={lamp.scene}
-                    /> */}
 
-                    <OrbitControls zoom0={true}></OrbitControls>
+                    <primitive scale={1} position={[0, -3, 0]} object={computers.scene} />
+
+                    {/* <OrbitControls zoom0={true}></OrbitControls> */}
                 </Suspense>
 
-                <Html occlude="blending" scale={0.3} transform position={[-3.21, 0.6, 1.28]} rotation={[0, 1.2, 0]}>
+                <Html
+                    occlude="blending"
+                    scale={0.038}
+                    transform
+                    position={[-3.24, 0.6, 1.36]}
+                    rotation={[0.0, 1.22, 0.0]}
+                    fov={75}
+                >
                     <Page></Page>
                 </Html>
             </Canvas>
