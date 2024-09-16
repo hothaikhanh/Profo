d64f01941a495f5566e7c5b7c699b7dbd0e7456c
Hồ Thái Khanh <hothaikhanh@gmail.com>
2024-09-16 22:28:50 +0700

    - Updated Camera Controls

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Environment, OrbitControls, Html, PerspectiveCamera, CameraControls } from "@react-three/drei";
 
-import { useState, useRef, Suspense } from "react";
+import { useState, useRef, Suspense, useEffect } from "react";
 import Page from "./components/Page/Page";
 import AboutPage from "./components/AboutPage/AboutPage";
 import HistoryPage from "./components/HistoryPage/HistoryPage";
 import ProjectPage from "./components/ProjectPage/ProjectPage";
 import ContactPage from "./components/ContactPage/ContactPage";
 import "./App.scss";
-import "./NavBar.scss";
 
 function App() {
     const computers = useLoader(GLTFLoader, "./src/assets/old_computers/scene.gltf");
@@ -32,41 +31,22 @@ function App() {
         page_6: { positionX: 2.64, positionY: -0.35, positionZ: 0.28, targetX: 3.46, targetY: -0.35, targetZ: -0.53 },
     };
     const screen_positions = {
-        screen_1: {},
-        screen_2: {},
-        screen_3: {},
-        screen_4: {},
-        screen_5: {},
-        screen_6: {},
-        screen_7: {
-            positionX: -3.8,
-            positionY: 1.91,
-            positionZ: -2.48,
-            targetX: 0.0,
-            targetY: 0.54,
-            targetZ: 0.0,
-        },
-        screen_8: {
-            positionX: 0.97,
-            positionY: 1.9,
-            positionZ: -4.02,
-            targetX: 0.0,
-            targetY: -0.1,
-            targetZ: 0.0,
-        },
-        screen_9: {
-            positionX: 4.52,
-            positionY: 1.9,
-            positionZ: -1.46,
-            targetX: 0.0,
-            targetY: -1.05,
-            targetZ: 0.0,
-        },
+        screen_1: { position: [-3.24, 0.61, 1.36], rotation: [0.0, 1.22, 0.0] },
+        screen_2: { position: [-2.56, -1.76, -0.44], rotation: [0.0, 1.09, 0.0] },
+        screen_3: { position: [-1.27, 0.11, -1.7], rotation: [0.0, 1.0, 0] },
+        screen_4: { position: [0.26, -0.86, -2.42], rotation: [0.0, 0.0, 0.0] },
+        screen_5: { position: [1.78, -2.0, -1.59], rotation: [0.0, -0.36, 0.0] },
+        screen_6: { position: [2.98, -0.35, -0.09], rotation: [0.0, -0.8, 0.0] },
+        screen_7: { position: [-3.8, 1.91, -2.48], rotation: [0.0, 0.54, 0.0] },
+        screen_8: { position: [0.97, 1.9, -4.02], rotation: [0.0, -0.1, 0.0] },
+        screen_9: { position: [4.52, 1.9, -1.46], rotation: [0.0, -1.05, 0.0] },
     };
     const [currentCameraSettings, setCurrentCameraSettings] = useState(camera_positions.default);
-    const DEG45 = Math.PI / 4;
+    const cameraControlRef = useRef();
 
     //DEV TOOLS
+    const devMode = false;
+    const allowMouse = true;
     const [devCameraSettings, setDevCameraSettings] = useState(currentCameraSettings);
     const [objectSettings, setObjectSettings] = useState({
         positionX: 0,
@@ -80,17 +60,36 @@ function App() {
     const devObjectRotation = [objectSettings.targetX, objectSettings.targetY, objectSettings.targetZ];
     //DEV TOOLS
 
+    function updateCamera(newSettings) {
+        cameraControlRef.current.setLookAt(
+            newSettings.positionX,
+            newSettings.positionY,
+            newSettings.positionZ,
+            newSettings.targetX,
+            newSettings.targetY,
+            newSettings.targetZ,
+            true
+        );
+    }
+
     return (
         <div id="canvas-container">
             <Canvas>
-                {/* <CameraControls enabled={true} makeDefault ref={cameraControlRef} /> */}
-                <ScrollCamera cameraSettings={currentCameraSettings} />
-                {/* <PerspectiveCamera position={[0, 0, 0]} /> */}
-                {/* <OrbitControls makeDefault></OrbitControls> */}
+                <CameraControls
+                    enabled={true}
+                    makeDefault
+                    ref={cameraControlRef}
+                    mouseButtons={{
+                        left: allowMouse ? 1 : null,
+                        right: allowMouse ? 2 : null,
+                        wheel: allowMouse ? 8 : null,
+                        middle: allowMouse ? 8 : null,
+                    }}
+                />
 
-                <ambientLight color="black" intensity={Math.PI} />
+                <ambientLight color="grey" intensity={Math.PI} />
                 <spotLight
-                    color="white"
+                    color="green"
                     position={[0, 5, 0]}
                     angle={1}
                     penumbra={0.4}
@@ -101,142 +100,151 @@ function App() {
                     <primitive scale={1} position={[0, -3, 0]} object={computers.scene} />
                 </Suspense>
 
+                {/* ABOUT ME PAGE */}
                 <Html
                     occlude="blending"
                     scale={0.038}
                     transform
-                    position={[-3.24, 0.61, 1.36]}
-                    rotation={[0.0, 1.22, 0.0]}
+                    position={screen_positions.screen_1.position}
+                    rotation={screen_positions.screen_1.rotation}
                     fov={75}
                 >
                     <Page title={"About me"}>
                         <AboutPage></AboutPage>
                     </Page>
                 </Html>
-
+                {/* WORK HISTORY PAGE */}
                 <Html
                     occlude="blending"
                     scale={0.041}
                     transform
-                    position={[-2.56, -1.76, -0.44]}
-                    rotation={[0.0, 1.09, 0.0]}
+                    position={screen_positions.screen_2.position}
+                    rotation={screen_positions.screen_2.rotation}
                     fov={75}
                 >
                     <Page title={"work history"}>
                         <HistoryPage></HistoryPage>
                     </Page>
                 </Html>
-
+                {/* PROJECTS 1 PAGE */}
                 <Html
                     occlude="blending"
                     scale={0.041}
                     transform
-                    position={[-1.27, 0.11, -1.7]}
-                    rotation={[0.0, 1.0, 0]}
+                    position={screen_positions.screen_3.position}
+                    rotation={screen_positions.screen_3.rotation}
                     fov={75}
                 >
                     <Page title={"my projects"}>
                         <ProjectPage></ProjectPage>
                     </Page>
                 </Html>
-
+                {/* PROJECTS 2 PAGE */}
                 <Html
                     occlude="blending"
                     scale={0.041}
                     transform
-                    position={[0.26, -0.86, -2.42]}
-                    rotation={[0.0, 0.0, 0.0]}
+                    position={screen_positions.screen_4.position}
+                    rotation={screen_positions.screen_4.rotation}
                     fov={75}
                 >
                     <Page title={"my projects"}>
                         <ProjectPage></ProjectPage>
                     </Page>
                 </Html>
-
+                {/* PROJECTS 3 PAGE */}
                 <Html
                     occlude="blending"
                     scale={0.041}
                     transform
-                    position={[1.78, -2.0, -1.59]}
-                    rotation={[0.0, -0.36, 0.0]}
+                    position={screen_positions.screen_5.position}
+                    rotation={screen_positions.screen_5.rotation}
                     fov={75}
                 >
                     <Page title={"my projects"}>
                         <ProjectPage></ProjectPage>
                     </Page>
                 </Html>
-
+                {/* CONTACT PAGE */}
                 <Html
                     occlude="blending"
                     scale={0.038}
                     transform
-                    position={[2.98, -0.35, -0.09]}
-                    rotation={[0.0, -0.8, 0.0]}
+                    position={screen_positions.screen_6.position}
+                    rotation={screen_positions.screen_6.rotation}
                     fov={75}
                 >
                     <Page hasFrame={false}>
                         <ContactPage></ContactPage>
                     </Page>
                 </Html>
-
-                {/* background screens */}
+                {/* EMPTY SCREENS */}
                 <Html
                     occlude="blending"
                     scale={0.041}
                     transform
-                    position={[-3.8, 1.91, -2.48]}
-                    rotation={[0.0, 0.54, 0.0]}
+                    position={screen_positions.screen_7.position}
+                    rotation={screen_positions.screen_7.rotation}
                     fov={75}
                 >
                     <Page hasFrame={false}></Page>
                 </Html>
-
                 <Html
                     occlude="blending"
                     scale={0.041}
                     transform
-                    position={[0.97, 1.9, -4.02]}
-                    rotation={[0.0, -0.1, 0.0]}
+                    position={screen_positions.screen_8.position}
+                    rotation={screen_positions.screen_8.rotation}
                     fov={75}
                 >
                     <Page hasFrame={false}></Page>
                 </Html>
-
                 <Html
                     occlude="blending"
                     scale={0.041}
                     transform
-                    position={[4.52, 1.9, -1.46]}
-                    rotation={[0.0, -1.05, 0.0]}
+                    position={screen_positions.screen_9.position}
+                    rotation={screen_positions.screen_9.rotation}
                     fov={75}
                 >
                     <Page hasFrame={false}></Page>
                 </Html>
-                {/* background screens */}
             </Canvas>
 
-            {/* LIVE CONTROLLER */}
+            {/* NAV BAR */}
             <div className="bottom-nav">
-                <button className="nav-btn" onClick={() => setCurrentCameraSettings(camera_positions.page_1)}>
+                <button className="nav-btn" onClick={() => updateCamera(camera_positions.default)}>
+                    Home
+                </button>
+                <button className="nav-btn" onClick={() => updateCamera(camera_positions.page_1)}>
                     About me
                 </button>
-                <button className="nav-btn" onClick={() => setCurrentCameraSettings(camera_positions.page_1)}>
-                    Skills
-                </button>
-                <button className="nav-btn" onClick={() => setCurrentCameraSettings(camera_positions.page_2)}>
+                <button className="nav-btn" onClick={() => updateCamera(camera_positions.page_2)}>
                     Work Experience
                 </button>
-                <button className="nav-btn" onClick={() => setCurrentCameraSettings(camera_positions.page_3)}>
+                <button className="nav-btn" onClick={() => updateCamera(camera_positions.page_3)}>
                     Projects
                 </button>
-                <button className="nav-btn" onClick={() => setCurrentCameraSettings(camera_positions.page_6)}>
+                <button className="nav-btn" onClick={() => updateCamera(camera_positions.page_6)}>
                     Contact
                 </button>
             </div>
 
             {/* DEV MODE CONTROLLERS*/}
-            {/* <ControlBoard name="Camera controller" property={devCameraSettings} setProperty={setDevCameraSettings} />
-            <ControlBoard name="Secondary Control Board" property={objectSettings} setProperty={setObjectSettings} /> */}
+            {devMode ? (
+                <>
+                    <ControlBoard
+                        name="Camera controller"
+                        property={devCameraSettings}
+                        setProperty={setDevCameraSettings}
+                    />
+                    <ControlBoard
+                        name="Secondary Control Board"
+                        property={objectSettings}
+                        setProperty={setObjectSettings}
+                    />
+                </>
+            ) : null}
         </div>
     );
 }
@@ -315,18 +323,4 @@ function PropController({ property, modifyKey, setProp, name }) {
             </div>
         </>
     );
-}
-
-function ScrollCamera({ cameraSettings }) {
-    const vec = new THREE.Vector3();
-    useFrame((state) => {
-        state.camera.lookAt(cameraSettings.targetX, cameraSettings.targetY, cameraSettings.targetZ);
-        state.camera.position.lerp(
-            vec.set(cameraSettings.positionX, cameraSettings.positionY, cameraSettings.positionZ),
-            0.02
-        );
-        state.camera.updateProjectionMatrix();
-    });
-
-    return null;
 }
