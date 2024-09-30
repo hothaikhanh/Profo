5e1b672328c4f31e7d046a2a97b213c487bdc37d
Hồ Thái Khanh <73004709+hothaikhanh@users.noreply.github.com>
2024-09-30 19:51:43 +0700

    - Updated Resources

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -19,7 +19,7 @@ export default function AboutPage({ data, skills }) {
                 <div className="side-content">
                     <div className="desc">
                         <div className="row">
-                            {/* <div className="title">{data.desc.title[lang]}</div> */}
+                            <div className="title">{data.desc.title[lang]}</div>
                             <div className="current-status">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                     <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
