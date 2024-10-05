71f20ac8af4ef5f749566e750b5a2859dc9b940d
Hồ Thái Khanh <hothaikhanh@gmail.com>
2024-10-05 18:11:18 +0700

    - Updated About page
    - Added download file button

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,6 +9,29 @@ export default function AboutPage({ data, skills }) {
 
     const lang = useContext(LanguageContext);
 
+    let handleDownload = () => {
+        let url = "/src/assets/pdf/" + (lang == "EN" ? "EN.pdf" : "VN.pdf");
+        let fileName = lang == "EN" ? "hothaikhanh-resume.pdf" : "hothaikhanh-cv.pdf";
+
+        fetch(url)
+            .then((response) => response.blob())
+            .then((blob) => {
+                const url = window.URL.createObjectURL(new Blob([blob]));
+                const link = document.createElement("a");
+                link.href = url;
+                link.download = fileName;
+                document.body.appendChild(link);
+
+                link.click();
+
+                document.body.removeChild(link);
+                window.URL.revokeObjectURL(url);
+            })
+            .catch((error) => {
+                console.error("Error fetching the file:", error);
+            });
+    };
+
     return (
         <div className="about-page">
             <div className="top-content ">
@@ -37,7 +60,7 @@ export default function AboutPage({ data, skills }) {
                         </div>
                     </div>
 
-                    <HoverButton>
+                    <HoverButton onClick={handleDownload}>
                         <div className="download-cv">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="20px">
                                 <path d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z" />
